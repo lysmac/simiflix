c9e2df1ccdfb33ea899a00254b5647309612b4a1
edvindjulic <djulicedvin@gmail.com>
2023-02-27 15:02:48 +0100

    Hard code Watchlist. Wrapper and CardWrapper with style

--- src/Watchlist.tsx
+++ src/Watchlist.tsx
@@ -1,10 +1,36 @@
+import styled from "styled-components";
+import SearchResultCard from "./SeachResultCard";
 
 export default function WatchList() {
   return (
-    <div>
+    <Wrapper>
       <h1>Watchlist goes here</h1>
-      <p>This is the WatchList component.</p>
-      
-    </div>
+      <MoviesWrapper>
+        <SearchResultCard />
+        <SearchResultCard />
+        <SearchResultCard />
+        <SearchResultCard />
+        <SearchResultCard />
+        <SearchResultCard />
+        <SearchResultCard />
+        <SearchResultCard />
+      </MoviesWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color: #14213d;
+`
+
+const MoviesWrapper = styled.div`
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+width: 100%;
+gap: 3rem;
+`
