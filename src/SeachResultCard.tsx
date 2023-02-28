08d11e8587056c3112ddd0232556a206d6358ac6
edvindjulic <djulicedvin@gmail.com>
2023-02-28 13:05:39 +0100

    Updated searchresultcard component

--- src/SeachResultCard.tsx
+++ src/SeachResultCard.tsx
@@ -4,35 +4,48 @@ import image from './assets/defaultPoster.jpg';
 export default function SearchResultCard () {
   return (
     <CardWrapper>
-      <MovieTitle>TITLE</MovieTitle>
-      <img src={image} alt="" />
-      <AddButton>+</AddButton>
+      <CardTitle>TITLE</CardTitle>
+      <CardImage>
+        <AddButton>+</AddButton>
+      </CardImage>
     </CardWrapper>
   )
 }
 
 const CardWrapper = styled.div`
-width: 15rem;
-display: flex;
-justify-content: center;
-flex-direction: column;
-border: 1px solid black;
-`
+  width: 15rem;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+`;
 
-// const MoviePoster = styled.img`
-// background-image: url('./assets/defaultPoster.jpg');
-// width: 100px;
-// height: 150px
-// `
+const CardTitle = styled.h3`
+  color: black;
+  font-weight: bold;
+  text-align: center;
+  margin: 0;
+  padding: 1rem;
+  background-color: #f8f8f8;
+`;
 
-const MovieTitle = styled.h3`
-color: black;
-font-weight: bold;
-text-align: center;
-`
+const CardImage = styled.div`
+  background-image: url(${image});
+  background-size: cover;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  height: 300px;
+  position: relative;
+`;
 
 const AddButton = styled.button`
-color: green;
-width: 50px;
-height: 30px;
-`
+  color: green;
+  width: 30px;
+  height: 20px;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin: 0.5rem;
+`;
+
