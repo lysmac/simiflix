c6ccce0c2362ca96bbc082c908e22d325b8c3a18
Jesper Lindström <108944899+Jesper-Lindstrom@users.noreply.github.com>
2023-03-01 10:38:10 +0100

    non-working. Added logic to searchbar. Working on making divs in searchresult based on searchQuery.
    Co-authored-by: Edvindjulic <Edvindjulic@users.noreply.github.com>

--- src/Searchbar.tsx
+++ src/Searchbar.tsx
@@ -1,25 +1,58 @@
-import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
+import SearchResultCard from "./SeachResultCard";
 
 // TODO: type på props
+
 export default function Searchbar(props: any) {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<any[]>([])
+
+  // function handleSearchClick() {
+  //   props.setIsSearched(!props.isSearched);
+  //   SearchResult();
+  // }
+
+  async function SearchResult() {
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=c0f1190bf24e1f667c8c22a047cfa712&query=${searchQuery}`);
+    const result = await response.json();
+    setSearchResults(result.results)
+    console.log(result);
+  }
+
   return (
     <StyledSearchbar>
-      <Form action="submit">
+      <Form onSubmit={(e) => {
+        e.preventDefault();
+        SearchResult();
+      }}
+      >
         <label htmlFor="search">Search Movie</label>
         <div>
-          <input type="text" id="search" placeholder="Searchbar" />
-          {/* <NavLink to="./searchresult">
-            <SubmitButton type="submit">Go</SubmitButton>
-          </NavLink> */}
-        </div>
+          <input 
+            type="text" 
+            id="search" 
+            placeholder="Searchbar"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)} />
+        </div> 
+        <SubmitButton>Go</SubmitButton>
       </Form>
-      <NavLink to="./searchresult">
-        <button onClick={() => props.setIsSearched(!props.isSearched)}>
-          Search
-        </button>
-      </NavLink>
-      <p>Searched is {props.isSearched.toString()}</p>
+      {searchResults.length > 0 && (
+        <ResultsContainer>
+          {searchResults.map((result => (
+            <SearchResultCard key={result.id}>
+              <Poster src={`https://image.tmdb.org/t/p/w185/${result.poster_path}`} />
+              <div>
+                <Title>{result.title}</Title>
+                <Tagline>{result.tagline}</Tagline>
+                <p>hej</p>
+              </div>
+            </SearchResultCard>
+          )))}
+        </ResultsContainer>
+      )},
+      {/* <p>Searched is {props.isSearched.toString()}</p> */}
     </StyledSearchbar>
   );
 }
@@ -46,3 +79,43 @@ const SubmitButton = styled.button`
   color: #fff;
   font-size: 1rem;
 `;
+
+
+//copy-pasta
+
+const ResultsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const ResultCard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  margin: 0.5rem;
+  padding: 1rem;
+  border-radius: 4px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+`;
+
+const Poster = styled.img`
+  width: 185px;
+  height: 278px;
+  object-fit: cover;
+  margin-right: 1rem;
+`;
+
+const Title = styled.h3`
+  margin: 0;
+`;
+
+const Tagline = styled.p`
+  margin: 0;
+`;
+
+
+
+
+
